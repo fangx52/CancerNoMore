1de9571092f4b03ad2c6cd0f3078998a2a233b76
Xuanyu Fang <47069547+fangx52@users.noreply.github.com>
2020-02-02 07:32:01 -0600

    backend

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,22 +1,34 @@
 from flask import render_template, request
 from app import app, mongo
-from comprehend import aws_comprehend
+import boto3
 
+@app.route('/')
+@app.route('/home', methods=["GET"])
+def home():
+    return render_template('home.html')
 @app.route('/C1L1', methods=["GET","POST"])
 def upload():
     error=''
-    try:
-        if request.method== "POST":
-            uploaded_txt= request.form.to_dict()['file1']
-            returned_txt= aws_comprehend(uploaded_txt)
-            # mongo.insert(returned_txt)
-            print(returned_txt)
-        return render_template('C1L1.html', title='Home')
-    except Exception as e:
-        return render_template("/C1L1", error=error)
-@app.route('/HOME', methods=["GET"])
-def home():
-    ##api request
-    ## api_ret
-    # x = 'hello there'
-    return render_template('HOME.html')
+    # try:
+    if request.method== "POST":
+        f = request.files['file']
+        f.save("test.txt")
+        file=open("test.txt","r")
+        text=''
+        for line in file:
+            text+=line
+        returned_txt= aws_comprehend(text)
+        for item in returned_txt['KeyPhrases']:
+            del item['BeginOffset']
+            del item['EndOffset']
+        # mongo.collection.insert(returned_txt)
+        print(returned_txt)
+    return render_template('C1L1.html', title='Home')
+    # except Exception as e:
+    #     return render_template("C1L1", error=e)
+
+
+def aws_comprehend(text):
+    comprehend_client=boto3.client('comprehend')
+    comprehend_response=comprehend_client.detect_key_phrases(Text=text, LanguageCode='en')
+    return comprehend_response
